fd963e48f694dfa2f731a9fe26e76aee87582cda
kaxi <kaxi@kaxideMacBook-Air.local>
2016-05-26 17:52:45 +0800

    Release 0.0.1.

---
+++ ECBLoginApp/ECBLoginController.m
@@ -0,0 +1,37 @@
+//
+//  ECBLoginController.m
+//  ECBLogin
+//
+//  Created by kaxi on 16/5/26.
+//  Copyright © 2016年 kaxi. All rights reserved.
+//
+
+#import "ECBLoginController.h"
+
+@interface ECBLoginController ()
+
+@end
+
+@implementation ECBLoginController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ ECBLoginApp/ECBRegisterController.h
@@ -0,0 +1,13 @@
+//
+//  ECBRegisterController.h
+//  ECBLogin
+//
+//  Created by kaxi on 16/5/26.
+//  Copyright © 2016年 kaxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECBRegisterController : UIViewController
+
+@end
